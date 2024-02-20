d583194162534c8aaa10f155694a6bffdca3e27b
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2024-02-20 22:17:50 +0000

    Add some sensible defaults, used to setup M3 Max MBP

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-user=$(stat -f %Su /dev/console);
-
-sudo -u $user defaults write com.apple.menuextra.battery -ShowPercent YES
+# Enable battery percentage on notification area.
+defaults -currentHost write com.apple.controlcenter.plist BatteryShowPercentage -bool true
+# Update Activity Monitor faster.
+defaults write com.apple.ActivityMonitor "UpdatePeriod" -int "2"
+# Show CPU percentage for each app on dock.
+defaults write com.apple.ActivityMonitor "IconType" -int "5"
+# Display notifications when music changes.
+defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "true"
+# Focus follows the mouse cursor to any Terminal windows.
+defaults write com.apple.Terminal "FocusFollowsMouse" -bool "true"
+# Group windows by application.
+defaults write com.apple.dock "expose-group-apps" -bool "true"
+# Dock icon size.
+defaults write com.apple.dock "tilesize" -int "36"
+# Dock position left.
+defaults write com.apple.dock "orientation" -string "left"
